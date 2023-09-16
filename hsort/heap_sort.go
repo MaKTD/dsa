089372c657e291876e4bbdc8f173fce765d283f6
maktd <tima98tima@gmail.com>
2023-09-16 12:55:03 +0300

    add comment to heap sort

--- hsort/heap_sort.go
+++ hsort/heap_sort.go
@@ -3,6 +3,7 @@ package hsort
 import "github.com/MaKTD/go-dsa/bheap"
 
 func HeapSort[T bheap.BinaryHeapElement[T]](slice []T) {
+	// is is actually possibly to do in place sort if binary heap is max based
 	h := bheap.New(slice...)
 
 	for i := 0; i < len(slice); i++ {
